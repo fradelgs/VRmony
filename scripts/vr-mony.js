aa49da8ef03afeacbdbdaa9a788a1f4788160b35
Valerio Maiolo <vm@MacBook-Pro-di-Valerio.local>
2022-05-29 23:26:58 +0200

    lattice gui raycasterPointermouse sound

--- scripts/vr-mony.js
+++ scripts/vr-mony.js
@@ -2,11 +2,20 @@ import * as THREE from './libs/three/three.module.js';
 import { BoxLineGeometry } from './libs/three/jsm/BoxLineGeometry.js';
 import { VRButton } from './libs/VRButton.js';
 import { XRControllerModelFactory } from './libs/three/jsm/XRControllerModelFactory.js';
+import { OrbitControls } from './libs/three/jsm/OrbitControls.js';
+import { GUI } from './libs/three/jsm/dat.gui.module.js';
 
-let camera, listener, scene, raycaster, renderer;
+
+
+let camera, listener, scene, raycaster, renderer, pointer, CLICKED;
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
 let room;
+
+let color = {
+	false: 'white', 
+	true: '0xff00ff'
+};
 
 const container = document.createElement( 'div' );
 document.body.appendChild( container );
@@ -31,16 +40,16 @@ function initScene(){
 
     // CAMERA
     camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 100 );
-    camera.position.set( 0, 1.6, 0 );
-    camera.lookAt( 0, 0, -2 );
+    camera.position.set( 0, 1.6, 8 );
+    camera.lookAt( 0, 0, 0 );
     camera.add(listener);
 
     // ROOM
     room = new THREE.LineSegments(
-        new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ),
+        new BoxLineGeometry( 20, 10, 20, 10, 10, 10 ),
         new THREE.LineBasicMaterial( { color: 0x808080 } )
     );
-    room.geometry.translate( 0, 3, 0 );
+    room.geometry.translate( 0, 5, 0 );
     scene.add(room);
 
     // LIGHT
@@ -49,6 +58,9 @@ function initScene(){
 	light.position.set( 1, 1, 1 ).normalize();
 	scene.add( light ); 
 
+    // LATTICE
+    initLattice();
+
     // RENDERER
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -56,7 +68,166 @@ function initScene(){
     renderer.outputEncoding = THREE.sRGBEncoding;
     document.body.appendChild( renderer.domElement );
 
+    //POINTER MOUSE
+    CLICKED = null;
+    pointer= new THREE.Vector2();
+
+    document.addEventListener( 'pointerdown', mouseDown, false );
+
     window.addEventListener('resize', onWindowResize );
+}
+
+function initLattice(){
+    var limitLattice = 3;
+	const intonation = [];
+	let fund = 261.6;
+	const d = 2;
+	var note = new Array(limitLattice);
+	var sound = new Array(limitLattice);
+	var oscillator = new Array(limitLattice);
+	var spherePosition = new Array(limitLattice);
+    let lattice;
+
+    for (var i = 0; i < limitLattice; i++) {
+		spherePosition[i] = new Array(limitLattice);
+		intonation[i] = new Array(limitLattice);
+		note[i] = new Array(limitLattice);
+		sound[i] = new Array(limitLattice);
+		oscillator[i] = new Array(limitLattice);
+	}
+
+	for(var i = 0; i<limitLattice; i++){
+		for(var j = 0; j<limitLattice; j++){
+			spherePosition[i][j] = new Array(limitLattice);
+			intonation[i][j] = new Array(limitLattice);
+			note[i][j] = new Array(limitLattice);
+			sound[i][j] = new Array(limitLattice);
+			oscillator[i][j] = new Array(limitLattice);
+		}
+	}
+
+	for(var i = 0; i<limitLattice; i++){
+		for(var j = 0; j<limitLattice; j++){
+			for(var k = 0; k<limitLattice; k++){
+				spherePosition[i][j][k] = [ i*d, j*d, k*d];
+				intonation[i][j][k] = ((fund * Math.pow(2, (i*7)/12)) * Math.pow(2, (j*4)/12))*Math.pow(2, (k*10)/12);
+			}
+		}
+	}
+
+    const vertex = new THREE.SphereGeometry(0.5, 30, 30);
+	
+	lattice = new THREE.Group();
+
+	const geometry = [];
+	const material = [];
+
+    for(var i = 0; i<limitLattice; i++){
+		for(var j = 0; j<limitLattice; j++){
+			for(var k = 0; k<limitLattice; k++){
+				note[i][j][k] = new THREE.Mesh( vertex, new THREE.MeshStandardMaterial( { color: 'white', roughness: 1, metalness: 0.5 } ) );
+				note[i][j][k].position.set(spherePosition[i][j][k][0],spherePosition[i][j][k][1],spherePosition[i][j][k][2]);
+				note[i][j][k].userData[0] = {MODEL: false, PREVIOUS: false};
+				note[i][j][k].name = "vertex";
+				note[i][j][k].uuid = [i, j, k];
+				sound[i][j][k] = new THREE.PositionalAudio( listener );
+				oscillator[i][j][k] = listener.context.createOscillator();
+				oscillator[i][j][k].type = 'sawtooth';
+				oscillator[i][j][k].frequency.setValueAtTime(intonation[i][j][k], listener.context.currentTime);
+				oscillator[i][j][k].start(0);
+				sound[i][j][k].setNodeSource(oscillator[i][j][k]);
+				sound[i][j][k].setRefDistance(20);  
+				sound[i][j][k].setVolume(0.0);
+				//sound[i][j][k].connect(filter);
+				note[i][j][k].add(sound[i][j][k]);
+				lattice.add(note[i][j][k]);
+			}
+		}
+	}
+
+    lattice.name = "LATTICE";
+    lattice.position.set(-0.5*(limitLattice),0.8,-0.5*(limitLattice + d)); // trovare position in f(limitLattice e distanza d)
+
+    scene.add( lattice );
+
+    // GUI
+    initGUI(limitLattice, oscillator);
+
+}
+
+function changeState(Object){
+	
+	if (Object.userData[0].MODEL==false) {
+		Object.userData[0].MODEL= true;
+		myRender(Object);	
+		
+		db.collection("state").doc("counter").set({
+			id_note: Object.uuid,
+			value: Object.userData[0].MODEL,
+			color: color[Object.userData[0].MODEL]
+		})
+	}
+	else {Object.userData[0].MODEL= false;
+		myRender(Object);	
+		db.collection("state").doc("counter").set({
+			id_note: Object.uuid,
+			value: Object.userData[0].MODEL,
+			color: color[Object.userData[0].MODEL]
+		})
+	}
+}	
+
+function myRender(Object){
+	Object.material.emissive.setHex( color[Object.userData[0].MODEL] );
+	if(Object.children[0]) Object.children[0].gain.gain.setTargetAtTime(Object.userData[0].MODEL*0.125, listener.context.currentTime + 0, 0.5);
+}
+
+function mouseDown(event) {
+	// find intersections
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+	raycaster.setFromCamera( pointer, camera );
+   
+
+    var intersectable = scene.getObjectByName("LATTICE", true);
+    console.log(intersectable)
+
+    const intersects = raycaster.intersectObjects( intersectable.children, true );
+
+	if ( intersects.length > 0){
+
+		CLICKED = intersects[ 0 ].object; // get clicked object
+
+		const id = CLICKED.uuid; //getID of clicked object
+		console.log("id " + id);
+		
+		changeState(CLICKED);
+	}
+}
+
+function initGUI(limitLattice, oscillator){
+    const GeneratorControls = function () {
+		//this.frequency = oscillator[0][0][0].frequency.value;
+		this.wavetype = oscillator[0][0][0].type;
+	};
+    const gui = new GUI();
+	const generatorControls = new GeneratorControls();
+	const generatorFolder = gui.addFolder( 'sound generator' );
+
+    generatorFolder.add( generatorControls, 'wavetype', [ 'sine', 'square', 'sawtooth', 'triangle' ] ).onChange( function () {
+
+		for(var i = 0; i<limitLattice; i++){
+			for(var j = 0; j<limitLattice; j++){
+				for(var k = 0; k<limitLattice; k++){
+					oscillator[i][j][k].type = generatorControls.wavetype;
+				}
+			}
+        }
+	} );
+
+    generatorFolder.open();
+
 }
 
 function setupVR(){
@@ -117,6 +288,9 @@ function setupVR(){
     raycaster = new THREE.Raycaster()
     const workingMatrix = new THREE.Matrix4();
     const origin = new THREE.Vector3();
+    const controls = new OrbitControls( camera, renderer.domElement );
+	controls.update();
+
 }
 
 function buildController( data ){
